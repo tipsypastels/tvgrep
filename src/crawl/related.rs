22ca40fbfff3045171044461c4a4f3cdaab042e2
tipsypastels <dakotasankey@gmail.com>
2025-02-24 12:39:19 -0800

    progress bar for listing crawler

--- src/crawl/related.rs
+++ src/crawl/related.rs
@@ -3,14 +3,17 @@ use crate::{
     name::{ArticleName, GroupName},
 };
 use anyhow::{Context, Result};
+use indicatif::{ProgressBar, ProgressStyle};
 use reqwest::Client;
-use scraper::Selector;
-use std::sync::LazyLock;
+use scraper::{Html, Selector};
+use std::sync::{LazyLock, OnceLock};
 use tracing::{Span, debug, field, instrument, trace};
+
+static COUNT_SEL: LazyLock<Selector> = LazyLock::new(|| Selector::parse("p > strong").unwrap());
 
 // // If we were signed in this would not work because there's an additional <a> to edit inside each <li>.
 // // TODO: Doesn't work without a group, no header.
-static LISTING_SELECTOR: LazyLock<Selector> =
+static LISTING_SEL: LazyLock<Selector> =
     LazyLock::new(|| Selector::parse(".examples-header + ul > li > a").unwrap());
 
 pub async fn crawl(
@@ -22,6 +25,7 @@ pub async fn crawl(
         client,
         article,
         group,
+        progress: OnceLock::new(),
     }
     .crawl()
     .await
@@ -31,6 +35,7 @@ struct RelatedCrawler<'a> {
     client: &'a Client,
     article: &'a ArticleName,
     group: Option<&'a GroupName>,
+    progress: OnceLock<CrawlerProgress>,
 }
 
 impl RelatedCrawler<'_> {
@@ -58,6 +63,10 @@ impl RelatedCrawler<'_> {
             page += 1;
         }
 
+        if let Some(progress) = self.progress.get() {
+            progress.finish();
+        }
+
         Ok(out)
     }
 
@@ -73,16 +82,61 @@ impl RelatedCrawler<'_> {
         debug!(url, "crawling listing");
 
         let html = super::scrape(self.client, &url).await?;
-        let links = html.select(&LISTING_SELECTOR);
+        let progress = self.get_or_init_progress(&html);
+        let links = html.select(&LISTING_SEL);
+
+        progress.set_page(page);
 
         for link in links {
             let url = link.attr("href").context("link has no url")?;
             let article = ArticleName::from_url(url).context("invalid url")?;
 
             trace!(%article);
+
             out.push_assume_sorted(article);
+            progress.inc();
         }
 
         Ok(())
     }
+
+    fn get_or_init_progress(&self, html: &Html) -> &CrawlerProgress {
+        self.progress.get_or_init(|| CrawlerProgress::new(html))
+    }
+}
+
+#[derive(Debug)]
+struct CrawlerProgress(Option<ProgressBar>);
+
+impl CrawlerProgress {
+    fn new(html: &Html) -> Self {
+        Self(find_total_count(html).map(create_progress))
+    }
+
+    fn set_page(&self, page: u8) {
+        if let Some(pb) = &self.0 {
+            pb.set_message(format!("Page {page}"));
+        }
+    }
+
+    fn inc(&self) {
+        if let Some(pb) = &self.0 {
+            pb.inc(1);
+        }
+    }
+
+    fn finish(&self) {
+        if let Some(pb) = &self.0 {
+            pb.finish_and_clear();
+        }
+    }
+}
+
+fn create_progress(len: u64) -> ProgressBar {
+    ProgressBar::new(len)
+        .with_style(ProgressStyle::with_template("{msg} {wide_bar} {pos}/{len}").unwrap())
+}
+
+fn find_total_count(html: &Html) -> Option<u64> {
+    html.select(&COUNT_SEL).next()?.text().next()?.parse().ok()
 }
