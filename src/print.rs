cc3529bd1d9fb6aef6286d141b1f2a8e55af52ef
tipsypastels <dakotasankey@gmail.com>
2025-03-04 14:58:39 -0800

    support Printer::print_async

--- src/print.rs
+++ src/print.rs
@@ -1,5 +1,9 @@
 use crate::name::{ArticleName, GroupName};
-use std::fmt::{self, Display};
+use futures::{Stream, StreamExt};
+use std::{
+    fmt::{self, Display},
+    pin::pin,
+};
 
 #[derive(Debug, Clone)]
 pub struct Printer<I> {
@@ -7,11 +11,7 @@ pub struct Printer<I> {
     unfiltered_len: Option<usize>,
 }
 
-impl<'a, I, P> Printer<I>
-where
-    I: Iterator<Item = &'a P>,
-    P: PrintArticleEntry + 'a,
-{
+impl<I> Printer<I> {
     pub fn new(iter: I) -> Self {
         Self {
             iter,
@@ -23,33 +23,77 @@ where
         self.unfiltered_len = Some(len);
         self
     }
+}
 
+impl<'a, I, P> Printer<I>
+where
+    I: Iterator<Item = &'a P>,
+    P: PrintArticleEntry + 'a,
+{
+    #[allow(unused)]
     pub fn print(self) {
-        let mut cur_group = None;
-        let mut cnt = 0usize;
+        let mut state = PrinterState::new();
 
         for entry in self.iter {
-            cnt += 1;
-
-            let group = entry.group();
-
-            if cur_group.is_none() || cur_group.is_some_and(|cg| cg != group) {
-                cur_group = Some(group);
-
-                println!();
-                println!("{group}");
-                println!();
-            }
-
-            println!("\t{}", entry.display());
+            state.advance(entry);
         }
 
-        println!();
-        println!("({cnt} results)");
+        state.finish(self.unfiltered_len);
+    }
+}
 
-        if let Some(unfiltered_len) = self.unfiltered_len {
-            let filtered_out = unfiltered_len - cnt;
-            println!("({filtered_out} results filtered out)")
+impl<'a, S, P> Printer<S>
+where
+    S: Stream<Item = &'a P>,
+    P: PrintArticleEntry + 'a,
+{
+    pub async fn print_async(self) {
+        let mut state = PrinterState::new();
+        let mut stream = pin!(self.iter);
+
+        while let Some(entry) = stream.next().await {
+            state.advance(entry);
+        }
+
+        state.finish(self.unfiltered_len);
+    }
+}
+
+struct PrinterState<'a> {
+    group: Option<&'a GroupName>,
+    count: usize,
+}
+
+impl<'a> PrinterState<'a> {
+    fn new() -> Self {
+        Self {
+            group: None,
+            count: 0,
+        }
+    }
+
+    fn advance<P: PrintArticleEntry>(&mut self, entry: &'a P) {
+        self.count += 1;
+
+        let group = entry.group();
+        if self.group.is_none() || self.group.is_some_and(|g| g != group) {
+            self.group = Some(group);
+
+            println!();
+            println!("{group}");
+            println!();
+        }
+
+        println!("\t{}", entry.display());
+    }
+
+    fn finish(self, unfiltered_len: Option<usize>) {
+        println!();
+        println!("({} results)", self.count);
+
+        if let Some(unfiltered_len) = unfiltered_len {
+            let filtered_out = unfiltered_len - self.count;
+            println!("({filtered_out} results filtered out)");
         }
     }
 }
@@ -69,24 +113,24 @@ impl PrintArticleEntry for ArticleName {
     }
 }
 
-impl<T> PrintArticleEntry for (ArticleName, T)
-where
-    T: Display,
-{
-    fn group(&self) -> &GroupName {
-        &self.0.group
-    }
+// impl<T> PrintArticleEntry for (ArticleName, T)
+// where
+//     T: Display,
+// {
+//     fn group(&self) -> &GroupName {
+//         &self.0.group
+//     }
 
-    fn display(&self) -> impl Display {
-        struct TupleDisplay<'a, T>(&'a ArticleName, &'a T);
-        impl<T> Display for TupleDisplay<'_, T>
-        where
-            T: Display,
-        {
-            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-                write!(f, "{} ({})", self.0.display_link(), self.1)
-            }
-        }
-        TupleDisplay(&self.0, &self.1)
-    }
-}
+//     fn display(&self) -> impl Display {
+//         struct TupleDisplay<'a, T>(&'a ArticleName, &'a T);
+//         impl<T> Display for TupleDisplay<'_, T>
+//         where
+//             T: Display,
+//         {
+//             fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//                 write!(f, "{} ({})", self.0.display_link(), self.1)
+//             }
+//         }
+//         TupleDisplay(&self.0, &self.1)
+//     }
+// }
