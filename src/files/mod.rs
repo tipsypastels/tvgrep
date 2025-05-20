c261244a7a181f83b3ba8eb8f83264cf80cb8f4d
tipsypastels <dakotasankey@gmail.com>
2025-05-20 16:31:29 -0700

    tvgrep_dev dir in dev

--- src/files/mod.rs
+++ src/files/mod.rs
@@ -10,6 +10,12 @@ pub use verdict::Verdict;
 
 static DIR: OnceCell<Box<Utf8Path>> = OnceCell::const_new();
 
+#[cfg(debug_assertions)]
+const DIR_NAME: &str = "tvgrep_dev";
+
+#[cfg(not(debug_assertions))]
+const DIR_NAME: &str = "tvgrep";
+
 async fn dir() -> Result<&'static Box<Utf8Path>> {
     DIR.get_or_try_init(async || {
         let data_dir: Utf8PathBuf = dirs::data_dir()
@@ -17,7 +23,7 @@ async fn dir() -> Result<&'static Box<Utf8Path>> {
             .try_into()
             .context("data dir not UTF-8")?;
 
-        let dir = data_dir.join("tvgrep").into_boxed_path();
+        let dir = data_dir.join(DIR_NAME).into_boxed_path();
 
         if !fs::try_exists(dir.as_ref()).await? {
             fs::create_dir(dir.as_ref())
